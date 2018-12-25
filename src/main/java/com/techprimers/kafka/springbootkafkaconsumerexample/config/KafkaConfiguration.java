d9bb74c647428bbcad90e9d3418c5cfcc73f3fc9
Sathish G <sathish.indin@gmail.com>
2018-12-25 22:22:13 +0530

    Integration with web app project

--- src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/config/KafkaConfiguration.java
+++ src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/config/KafkaConfiguration.java
@@ -39,23 +39,23 @@ public class KafkaConfiguration {
     }
 
 
-    @Bean
-    public ConsumerFactory<String, User> userConsumerFactory() {
-        Map<String, Object> config = new HashMap<>();
-
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_json");
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
-                new JsonDeserializer<>(User.class));
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, User> userKafkaListenerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, User> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(userConsumerFactory());
-        return factory;
-    }
+//    @Bean
+//    public ConsumerFactory<String, User> userConsumerFactory() {
+//        Map<String, Object> config = new HashMap<>();
+//
+//        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+//        //config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_json");
+//        //config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        //config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+//        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
+//                new JsonDeserializer<>(User.class));
+//    }
+//
+//    @Bean
+//    public ConcurrentKafkaListenerContainerFactory<String, User> userKafkaListenerFactory() {
+//        ConcurrentKafkaListenerContainerFactory<String, User> factory = new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(userConsumerFactory());
+//        return factory;
+//    }
 
 }
--- src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/listener/KafkaConsumer.java
+++ src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/listener/KafkaConsumer.java
@@ -7,15 +7,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "Kafka_Example", group = "group_id")
+    @KafkaListener(topics = "light", group = "group_id")
     public void consume(String message) {
         System.out.println("Consumed message: " + message);
     }
 
 
-    @KafkaListener(topics = "Kafka_Example_json", group = "group_json",
+/*    @KafkaListener(topics = "light", group = "group_json",
             containerFactory = "userKafkaListenerFactory")
     public void consumeJson(User user) {
         System.out.println("Consumed JSON Message: " + user);
-    }
+    }*/
 }
